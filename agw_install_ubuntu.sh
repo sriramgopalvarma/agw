51cf231be7ef17eb27f7b391e6207423a91d1b4a
sriramgopalvarma <121871187+sriramgopalvarma@users.noreply.github.com>
2023-12-06 13:24:15 +0530

    Update agw_install_ubuntu.sh

--- agw_install_ubuntu.sh
+++ agw_install_ubuntu.sh
@@ -57,7 +57,7 @@ INTERFACES=$(ip -br a)
 if [[ $1 != "$CLOUD_INSTALL" ]] && ( [[ ! $INTERFACES == *'eth0'*  ]] || [[ ! $INTERFACES == *'eth1'* ]] || ! grep -q 'GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"' /etc/default/grub); then
   # changing intefaces name
   sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
-  sed -i 's/enp0s3/eth0/g' /etc/netplan/50-cloud-init.yaml
+  sed -i 's/enp1s0/eth0/g' /etc/netplan/50-cloud-init.yaml
   # changing interface name
   grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -71,47 +71,15 @@ if [[ $1 != "$CLOUD_INSTALL" ]] && ( [[ ! $INTERFACES == *'eth0'*  ]] || [[ ! $I
   mkdir -p "$INTERFACE_DIR"
   echo "source-directory $INTERFACE_DIR" > /etc/network/interfaces
 
-  if [ -z "$addr1" ] || [ -z "$gw_addr" ]
-  then
-    # DHCP allocated interface IP
-    echo "auto eth0
-    iface eth0 inet dhcp" > "$INTERFACE_DIR"/eth0
-  else
-    # Statically allocated interface IP
-    if ipcalc -c "$addr1" | grep INVALID
-    then
-      echo "Interface ip is not valid IP"
-      exit 1
-    fi
-
-    if ipcalc -c "$gw_addr" | grep INVALID
-    then
-      echo "Upstream Router ip is not valid IP"
-      exit 1
-    fi
-
-    addr=$(   ipcalc -n "$addr1"  | grep Address | awk '{print $2}')
-    netmask=$(ipcalc -n "$addr1"  | grep Netmask | awk '{print $2}')
-    gw_addr=$(ipcalc -n "$gw_addr"| grep Address | awk '{print $2}')
-
-    echo "auto eth0
-  iface eth0 inet static
-  address $addr
-  netmask $netmask
-  gateway $gw_addr" > "$INTERFACE_DIR"/eth0
-  fi
-
   # configuring eth1
   echo "auto eth1
-  iface eth1 inet dhcp" > "$INTERFACE_DIR"/eth1
+  iface eth1 inet static
+  address 10.0.2.1
+  netmask 255.255.255.0" > "$INTERFACE_DIR"/eth1
 
-  # get rid of netplan
   systemctl unmask networking
   systemctl enable networking
-
-  apt-get --assume-yes purge nplan netplan.i
-
-  # Setting REBOOT flag to 1 because we need to reload new interface and network services.
+  
   NEED_REBOOT=1
 else
   echo "Interfaces name are correct, let's check if network and DNS are up"
